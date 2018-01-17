90c67ba5b2ceff1a4d09f909c0fd08fc09d0a363
MatheusDemiro <33012830+MatheusDemiro@users.noreply.github.com>
2018-01-17 04:53:41 -0200

    Login e Registro
    Código atualizado.

---
+++ App/spudydir/app/src/main/java/com/example/spudydev/spudy/infraestrutura/utils/UsuarioException.java
@@ -0,0 +1,11 @@
+package com.example.spudydev.spudy.infraestrutura.utils;
+
+/**
+ * Created by User on 15/01/2018.
+ */
+
+public class UsuarioException extends Exception {
+    public UsuarioException(String menssagem){
+        super(menssagem);
+    }
+}
